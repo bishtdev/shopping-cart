51f319c71cfcd274e1b14123158d8a096ccad2cc
bishtdev <bishtdev030@gmail.com>
2024-05-27 12:12:43 +0530

    setting up the navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import {BrowserRouter as Router , Routes, Route} from 'react-router-dom'
+import Navbar from './Components/Navbar'
 
 
 function App() {
   return (
     <div className='App'>
       <Router>
+        <Navbar/>
         <Routes>
           <Route path='/'/>
           <Route path='/cart'/>
